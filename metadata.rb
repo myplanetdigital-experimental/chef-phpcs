88cd4db1f5d5a6670f21b979a83e67bb7b8a9cb1
Patrick Connolly <patrick.c.connolly@gmail.com>
2011-09-15 15:20:41 -0400

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Myplanet Digital"
+maintainer_email "patrick@myplanetdigital.com"
+license          "Apache 2.0"
+description      "Installs/Configures phpcs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "php"

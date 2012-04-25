50d16ac3d41adf346387183576326105c7c46540
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-25 12:19:55 -0400

    version bump to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "patrick@myplanetdigital.com"
 license          "Apache 2.0"
 description      "Installs/Configures phpcs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "1.0.0"
 
 depends "php"
